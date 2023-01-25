55f78e0650b2dbab4f5911748dfa26f21cfe3234
Trevor French <trevorf96@gmail.com>
2023-01-25 14:22:54 -0700

    ui

--- README.md
+++ README.md
@@ -28,5 +28,8 @@ rsconnect deploy shiny ./app --name trevorfrench --title nyc-tax-liens
 
 You may need to delete the pycache dir if you get errors
 
+## Data
+https://data.cityofnewyork.us/City-Government/Tax-Lien-Sale-Lists/9rz4-mjek
+
 ## ACTION LIST
 - Create a separate requirements.txt doc for dev and keep one for deployment
--- app/app.py
+++ app/app.py
@@ -1,25 +1,32 @@
 """Main components."""
 # from pprint import pformat
 from shiny import App, render, ui
+from shiny.ui import TagList, div, h3, head_content, tags
+from pathlib import Path
 import requests
 import json
 import pandas as pd
 
 app_ui = ui.page_fluid(
-    ui.panel_title("NYC Tax Lien Sales"),
-    ui.input_numeric("limit", "Number of Results", value=25, max=50000),
-    ui.input_numeric("offset", "Page Number", value=1),
+    head_content(
+        # tags.meta(name="viewport", content="width=device-width, initial-scale=1.0"),
+        tags.style((Path(__file__).parent / "style.css").read_text()),
+    ),
+    div(
+        h3("NYC Tax Lien Sales"),
+        class_="title",
+    ),
+    div(
+        ui.input_numeric("limit", "Number of Results", value=25, min=1, max=50000),
+        ui.input_numeric("offset", "Page Number", value=1),
+        class_="filters",
+    ),
     ui.output_table("info"),
 )
 
 
 def server(input, output, session):
     """Server function."""
-    # @reactive.Calc
-    # def offset():
-    #     return (input.offset() - 1) * input.limit()
-
-    # @reactive.Calc
     def lien_data(limit=1500, offset=0):
         payload = {'$limit': limit, '$offset': offset}
         url = "https://data.cityofnewyork.us/resource/9rz4-mjek.json"
@@ -33,9 +40,9 @@ def server(input, output, session):
         limit = input.limit()
         offset = (input.offset() - 1) * input.limit()
         data = lien_data(limit, offset)
-        # print(data)
         df = pd.DataFrame(data)
-        print(df)
+        df['month'] = pd.to_datetime(df['month'], format="%Y-%m-%dT%H:%M:%S")
+        # print(df['month'])
         return df
 
 
